345deecaf384e436cad11594d9da3d6f6756cce7
ZunairaAhmad <bitf19a016@pucit.edu.pk>
2023-02-15 12:07:18 -0800

    Activity2 creation

--- app/src/main/java/com/example/mcqs_dbapp/APP_BO.java
+++ app/src/main/java/com/example/mcqs_dbapp/APP_BO.java
@@ -2,8 +2,8 @@ package com.example.mcqs_dbapp;
 
 public class APP_BO {
     private int question;
-    private int score;
-    private boolean answer;
+    private String score;
+    private String answer;
 
     public APP_BO(int question, String score, String answer) {
         this.question = question;
@@ -19,24 +19,24 @@ public class APP_BO {
         this.question = quest;
     }
 
-    public int getScore(){
+    public String getScore(){
         return score;
     }
 
-    public void setScore(int sco){
+    public void setScore(String sco){
         this.score = sco;
     }
 
-    public boolean getAnswer(){
+    public String getAnswer(){
         return answer;
     }
 
-    public void setAnswer(boolean ans){
+    public void setAnswer(String ans){
         answer = ans;
     }
 
     @Override
     public String toString() {
-        return "APP_BO {question=" + question + ", score=" + score + ", answer=" + answer + "]";
+        return "{Question=" + question + ", Your input=" + score + ", Correct answer=" + answer + "}";
     }
 }
---
+++ app/src/main/java/com/example/mcqs_dbapp/MainActivity2.java
@@ -0,0 +1,67 @@
+package com.example.mcqs_dbapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ImageButton whatsapp, github, result;
+    TextView wapp, hub, res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        DBHandler hand = new DBHandler(this);
+
+        whatsapp = (ImageButton) findViewById(R.id.whatappButton);
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ArrayList<String> arrayList = hand.selectAllStudents();
+                String text = "";
+                for (int i = 0; i < arrayList.size(); i++) text += arrayList.get(i) + "\n";
+                PackageManager pm = MainActivity2.this.getPackageManager();
+                try {
+                    Intent sendIntent = new Intent("android.intent.action.MAIN");
+                    //sendIntent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Conversation"));
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.setType("text/plain");
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+                    //sendIntent.putExtra("jid","03244910193@s.whatsapp.net"); //phone number without "+" prefix
+                    sendIntent.setPackage("com.whatsapp");
+                    startActivity(sendIntent);
+                } catch(Exception e) {
+                    Toast.makeText(MainActivity2.this, "WhatsApp not Installed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        github = (ImageButton) findViewById(R.id.githubButton);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri webpage = Uri.parse("https://github.com/ZunairaAhmad/MCQS_DBApp.git");
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent1);
+            }
+        });
+
+    }
+}
